60ca368eb2e9918d55fe42b620e7a2104d2174a9
Sunny Ghosh <sunny@sunnys-mbp.home>
2018-09-15 20:40:49 +0100

    ui- initial commit

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -4,8 +4,8 @@ const Web3 = require('web3');
 const compiledFactory = require('./build/CampaignFactory.json');
 
 const provider = new HDWalletProvider(
-    'seed words',
-    'https://rinkeby.infura.io/v3/token'
+    'transfer bean loan panic copy achieve wrong wave away pumpkin dial space',
+    'https://rinkeby.infura.io/v3/c39f9c9e5aa4480ca5f77ece82afb4a3'
 );
 
 const web3 = new Web3(provider);
@@ -16,7 +16,7 @@ const deploy = async () => {
 
     await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
     .deploy({ data : '0x' + compiledFactory.bytecode})
-    .send({gas: '5000000', from : 'address'})
+    .send({gas: '5000000', from : '0x2fd72bd1A59342F93aEb6F3e01f6bDb8783916e9'})
     .then((instance) => { 
         console.log("Contract mined at " + instance.options.address);
     });;
